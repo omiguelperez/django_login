1f04bdd5bbb46f09a31629e9a3e3a42e4ca56038
Oscar Pérez <mr.omiguelperez@gmail.com>
2017-07-09 17:47:07 -0500

    Sistema de usuarios básico

--- django_login/userprofiles/urls.py
+++ django_login/userprofiles/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from django.urls import reverse
 
 from . import views
 
@@ -7,4 +9,5 @@ urlpatterns = [
     url(r'^home/', views.home, name='home'),
     url(r'^login/', views.authentication, name='authentication'),
     url(r'^signup/', views.signup, name='signup'),
+    url(r'^logout/', auth_views.logout, {'next_page': '/login/'}, name='logout'),
 ]
--- django_login/userprofiles/views.py
+++ django_login/userprofiles/views.py
@@ -1,16 +1,31 @@
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
 
 
+@login_required(login_url='/login/')
 def home(request):
     return render(request, 'home.html', {})
 
 
 def authentication(request):
+    if request.method == 'POST':
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+        user = authenticate(username=username, password=password)
+        if user and user.is_authenticated():
+            login(request, user)
+            return redirect(reverse('home'))
     return render(request, 'login.html', {})
 
 
 def signup(request):
+    if request.method == 'POST':
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+        user = User.objects.create_user(username=username, password=password)
+        login(request, user)
+        return redirect(reverse('home'))
     return render(request, 'signup.html', {})
-
